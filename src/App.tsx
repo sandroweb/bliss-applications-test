dfd173228d74692294015fba9d3cf9315e167fd5
Jonas Mello <jonas.mello@dbccompany.com.br>
2022-02-02 09:14:37 -0300

    Remove unnecessary files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ut,
+      ullam dolores quia sunt est mollitia blanditiis non fuga quae sit alias,
+      quaerat in? Nobis fugit mollitia soluta suscipit illo.
     </div>
   );
 }
