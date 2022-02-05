773a96cd51266d2b29a0effa441c3f6fa3f162e9
Jonas Mello <jonas.mello@dbccompany.com.br>
2022-02-05 00:39:11 -0300

    Add loading component.

---
+++ src/components/loading/loading.styled.ts
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+import { LoadingProps } from './loading.type';
+
+export const StyledLoading = styled.div<LoadingProps>`
+  @keyframes grow {
+    from {
+      transform: scale(0, 0);
+      opacity: 0;
+    }
+    to {
+      transform: scale(1, 1);
+      opacity: 1;
+    }
+  }
+
+  @keyframes move {
+    from {
+      transform: translateX(0px);
+    }
+    to {
+      transform: translateX(45px);
+    }
+  }
+
+  opacity: ${({ show }) => (show ? 1 : 0)};
+  transition: opacity 300ms ease-in-out;
+
+  .container {
+    height: 15px;
+    display: flex;
+    margin: 0 auto;
+    width: 105px;
+    position: relative;
+  }
+  .circle {
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    background-color: ${({ theme }) => theme.palette.text.primary};
+    animation: move 500ms linear 0ms infinite;
+    margin-right: 30px;
+
+    &:first-child {
+      position: absolute;
+      top: 0;
+      left: 0;
+      animation: grow 500ms linear 0ms infinite;
+    }
+
+    &:last-child {
+      position: absolute;
+      top: 0;
+      right: 0;
+      margin-right: 0;
+      animation: grow 500ms linear 0s infinite reverse;
+    }
+  }
+`;
