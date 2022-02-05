a4de6f741169fba58fb281b1316bbbded4d92000
Jonas Mello <jonas.mello@dbccompany.com.br>
2022-02-05 17:50:56 -0300

    Add box shadow in card component.

--- src/components/card/card.styled.ts
+++ src/components/card/card.styled.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const StyledCard = styled.div`
   background: ${({ theme }) => theme.palette.common.white};
   border-radius: 5px;
+  box-shadow: 0 0 1px 0 ${({ theme }) => theme.palette.text.secondary};
   margin-bottom: 15px;
   padding: 15px;
 `;
