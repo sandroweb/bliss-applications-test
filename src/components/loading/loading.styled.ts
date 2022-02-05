e8356b80bc3a8448535106396016edfe75de3292
Jonas Mello <jonas.mello@dbccompany.com.br>
2022-02-05 01:35:01 -0300

    Loading component keyframes refactor.

--- src/components/loading/loading.styled.ts
+++ src/components/loading/loading.styled.ts
@@ -1,28 +1,28 @@
-import styled from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 
 import { LoadingProps } from './loading.type';
 
+const grow = keyframes`
+  from {
+    transform: scale(0, 0);
+    opacity: 0;
+  }
+  to {
+    transform: scale(1, 1);
+    opacity: 1;
+  }
+`;
+
+const move = keyframes`
+  from {
+    transform: translateX(0px);
+  }
+  to {
+    transform: translateX(45px);
+  }
+`;
+
 export const StyledLoading = styled.div<LoadingProps>`
-  @keyframes grow {
-    from {
-      transform: scale(0, 0);
-      opacity: 0;
-    }
-    to {
-      transform: scale(1, 1);
-      opacity: 1;
-    }
-  }
-
-  @keyframes move {
-    from {
-      transform: translateX(0px);
-    }
-    to {
-      transform: translateX(45px);
-    }
-  }
-
   opacity: ${({ show }) => (show ? 1 : 0)};
   transition: opacity 300ms ease-in-out;
 
@@ -38,14 +38,18 @@ export const StyledLoading = styled.div<LoadingProps>`
     height: 15px;
     border-radius: 50%;
     background-color: ${({ theme }) => theme.palette.text.primary};
-    animation: move 500ms linear 0ms infinite;
+    animation: ${css`
+      ${move} 500ms linear 0ms infinite
+    `};
     margin-right: 30px;
 
     &:first-child {
       position: absolute;
       top: 0;
       left: 0;
-      animation: grow 500ms linear 0ms infinite;
+      animation: ${css`
+        ${grow} 500ms linear 0ms infinite
+      `};
     }
 
     &:last-child {
@@ -53,7 +57,9 @@ export const StyledLoading = styled.div<LoadingProps>`
       top: 0;
       right: 0;
       margin-right: 0;
-      animation: grow 500ms linear 0s infinite reverse;
+      animation: ${css`
+        ${grow} 500ms linear 0s infinite reverse
+      `};
     }
   }
 `;
