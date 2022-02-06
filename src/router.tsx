943c4f0a7f96673f062a34af88de035714a709c3
Jonas Mello <jonas.mello@dbccompany.com.br>
2022-02-06 15:35:22 -0300

    Add blank page to route /.

--- src/router.tsx
+++ src/router.tsx
@@ -1,24 +1,15 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { App } from 'App';
-import { Questions } from 'pages/questions/questions';
+import { Questions } from 'pages/questions';
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <App>
         <Routes>
-          <Route path="/" element={<Questions />} />
+          <Route path="/" element={<></>} />
           <Route path="/questions" element={<Questions />} />
-          <Route
-            path="/questions/:id"
-            element={
-              <div>
-                <h1>Detail</h1>
-                <Link to={'/'}>Back to List</Link>
-              </div>
-            }
-          />
         </Routes>
       </App>
     </BrowserRouter>
