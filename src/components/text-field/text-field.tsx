df5f8d00f80778285af50c2830c40a6e2327be71
Jonas Mello <jonas.mello@dbccompany.com.br>
2022-02-06 18:02:53 -0300

    Add forwardRef hook to text field component.

--- src/components/text-field/text-field.tsx
+++ src/components/text-field/text-field.tsx
@@ -1,6 +1,10 @@
+import { forwardRef } from 'react';
+
 import { StyledInput } from './text-field.styled';
 import { TextFieldProps } from './text-field.type';
 
-export const TextField = (props: TextFieldProps) => {
-  return <StyledInput type="text" {...props} />;
-};
+export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
+  (props, ref) => {
+    return <StyledInput {...props} type="text" ref={ref} />;
+  }
+);
