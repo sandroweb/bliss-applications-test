45bb8b67d47d481af6e19f581825e59151ecd553
Jonas Mello <jonas.mello@dbccompany.com.br>
2022-02-08 13:27:03 -0300

    Add useFetchQuestions.

--- src/pages/questions/questions.tsx
+++ src/pages/questions/questions.tsx
@@ -1,15 +1,9 @@
-import { FC, useCallback, useEffect, useRef, useState } from 'react';
+import { FC } from 'react';
 import { AiOutlineArrowLeft, AiOutlineShareAlt } from 'react-icons/ai';
-import { useNavigate, useSearchParams } from 'react-router-dom';
-
-import { getQuestions } from 'api/questions/service';
-import { Question } from 'api/questions/types';
 
 import { Button } from 'components/button';
 import { QuestionList } from 'components/question-list';
 import { QuestionListHeader } from 'components/question-list-header';
-
-import { useDebounce } from 'helpers/useDebounce';
 
 import {
   Container,
@@ -17,59 +11,20 @@ import {
   SearchResultDescription,
   LoadMore,
 } from './questions.styled';
+import { useFetchQuestions } from './useFetchQuestions';
 
 export const Questions: FC = () => {
-  const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams({});
-  const [searchTerm, setSearchTerm] = useState<string | null>(
-    searchParams.get('filter')
-  );
-  const debouncedSearchTerm = useDebounce<string>(searchTerm || '', 400);
-  const [questions, setQuestions] = useState<Question[]>([]);
-  const [isSearching, setIsSearching] = useState<boolean>();
-  const [page, setPage] = useState(0);
-
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  const isSerachPage = searchTerm !== null;
-
-  const backToList = () => {
-    setSearchTerm(null);
-    navigate('/questions');
-  };
-
-  const fetchQuestions = useCallback(
-    (currentPage = 0) => {
-      setPage(currentPage);
-      getQuestions({ page: currentPage, filter: debouncedSearchTerm })
-        .then((response) => {
-          if (currentPage === 0) {
-            setQuestions(response.data);
-          } else {
-            setQuestions((prevQuestions) =>
-              prevQuestions.concat(response.data)
-            );
-          }
-        })
-        .finally(() => {
-          setIsSearching(false);
-        });
-    },
-    [debouncedSearchTerm]
-  );
-
-  useEffect(() => {
-    fetchQuestions();
-  }, [fetchQuestions]);
-
-  useEffect(() => {
-    if (isSerachPage) {
-      setSearchParams({ filter: searchTerm });
-    }
-    if (inputRef.current && isSerachPage && searchTerm === '') {
-      inputRef.current.focus();
-    }
-  }, [isSerachPage, searchTerm, setSearchParams]);
+  const {
+    isSerachPage,
+    searchTerm,
+    setSearchTerm,
+    inputRef,
+    backToList,
+    questions,
+    isSearching,
+    page,
+    fetchQuestions,
+  } = useFetchQuestions();
 
   return (
     <>
@@ -97,7 +52,7 @@ export const Questions: FC = () => {
               Back to List
             </Button>
             <SearchResultDescription>
-              <p>{`Search results for "${searchTerm.trim()}"`} </p>
+              <p>{`Search results for "${searchTerm?.trim()}"`} </p>
               <Button color="primary" iconEnd={<AiOutlineShareAlt />}>
                 Share
               </Button>
---
+++ src/pages/questions/useFetchQuestions.ts
@@ -0,0 +1,73 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+import { getQuestions } from 'api/questions/service';
+import { Question } from 'api/questions/types';
+
+import { useDebounce } from 'helpers/useDebounce';
+
+export const useFetchQuestions = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams({});
+  const [searchTerm, setSearchTerm] = useState<string | null>(
+    searchParams.get('filter')
+  );
+  const debouncedSearchTerm = useDebounce<string>(searchTerm || '', 400);
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [isSearching, setIsSearching] = useState<boolean>();
+  const [page, setPage] = useState(0);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const isSerachPage = searchTerm !== null;
+
+  const backToList = () => {
+    setSearchTerm(null);
+    navigate('/questions');
+  };
+
+  const fetchQuestions = useCallback(
+    (currentPage = 0) => {
+      setPage(currentPage);
+      getQuestions({ page: currentPage, filter: debouncedSearchTerm })
+        .then((response) => {
+          if (currentPage === 0) {
+            setQuestions(response.data);
+          } else {
+            setQuestions((prevQuestions) =>
+              prevQuestions.concat(response.data)
+            );
+          }
+        })
+        .finally(() => {
+          setIsSearching(false);
+        });
+    },
+    [debouncedSearchTerm]
+  );
+
+  useEffect(() => {
+    fetchQuestions();
+  }, [fetchQuestions]);
+
+  useEffect(() => {
+    if (isSerachPage) {
+      setSearchParams({ filter: searchTerm });
+    }
+    if (inputRef.current && isSerachPage && searchTerm === '') {
+      inputRef.current.focus();
+    }
+  }, [isSerachPage, searchTerm, setSearchParams]);
+
+  return {
+    isSerachPage,
+    searchTerm,
+    setSearchTerm,
+    inputRef,
+    backToList,
+    questions,
+    isSearching,
+    page,
+    fetchQuestions,
+  };
+};
